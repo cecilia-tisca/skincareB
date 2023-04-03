087e1fc9b9075c429051603baaf08cd040b7bfc4
Diana <190184@utags.edu.mx>
2023-04-03 17:38:16 -0500

    Diseño de la presentacion y primer apartado

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+  dataUser: any;
 
-}
+  constructor(private afAuth: AngularFireAuth, 
+    private router: Router) {
+
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    logOut () {
+      this.afAuth.signOut().then(() => this.router.navigate(['/login']))
+    }
+
+    
+    }
+
